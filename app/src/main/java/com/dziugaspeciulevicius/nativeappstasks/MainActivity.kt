ea5c01ce69660a3cd75f6ab6522bcd6a839cde60
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2020-11-30 21:37:23 +0200

    completing task1

---
+++ app/src/main/java/com/dziugaspeciulevicius/nativeappstasks/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dziugaspeciulevicius.nativeappstasks
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val db = FirebaseFirestore.getInstance()
+    }
+}
---
+++ app/src/main/java/com/dziugaspeciulevicius/nativeappstasks/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dziugaspeciulevicius.nativeappstasks
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 2000 // 2 secs splash load
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
